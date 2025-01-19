c1c56d72fc3cafeffa8987a11a07512e0d4c803e
宵夜-Kotlin <1640432@qq.com>
2025-01-19 14:37:53 +0800

    新增了主页入口

--- app/src/main/java/com/syc/world/Home.kt
+++ app/src/main/java/com/syc/world/Home.kt
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,6 +36,18 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun Home() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        LatestContentShow()
+    }
+}
 
 @Composable
 fun HeadlineInLargePrint(headline: String) {
@@ -71,78 +88,101 @@ fun LatestContentShow() {
             verticalArrangement = Arrangement.Center
         ) {
             HeadlineInLargePrint(headline = "最新动态")
-                OutlinedCard(
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surface,
-                    ),
-                    border = BorderStroke(1.dp, Color.Black),
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(10.dp))
-                        .clickable {
-                            // TODO
-                        }
-                ) {
-                    Box(
-                        modifier = Modifier
-
-                    ) {
-                        OutlinedCard(
-                            colors = CardDefaults.cardColors(
-                                containerColor = MaterialTheme.colorScheme.surface,
-                            ),
-                            border = BorderStroke(1.dp, Color.Black),
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(10.dp))
-                        ) {
-                            Row(
-                                modifier = Modifier,
-                                horizontalArrangement = Arrangement.Start,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Image(
-                                    modifier = Modifier
-                                        .padding(start = 10.dp)
-                                        .size(10.dp),
-                                    painter = painterResource(id = R.drawable.point_green),
-                                    contentDescription = null
-                                )
-                                Text(
-                                    text = "沉莫",
-                                    modifier = Modifier
-                                        .padding(10.dp),
-                                    textAlign = TextAlign.Center,
-                                    fontSize = 20.sp,
-                                    color = MaterialTheme.colorScheme.onBackground,
-                                    style = TextStyle(fontStyle = FontStyle.Normal)
-                                )
-                            }
-                        }
+            OutlinedCard(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                ),
+                border = BorderStroke(1.dp, Color.Black),
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable {
+                        // TODO
                     }
-                    Box(
+            ) {
+                Box(
+                    modifier = Modifier
+
+                ) {
+                    OutlinedCard(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surface,
+                        ),
+                        border = BorderStroke(1.dp, Color.Black),
                         modifier = Modifier
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
+                            .clip(RoundedCornerShape(10.dp))
                     ) {
-                        Text(
-                            text = "我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...",
-                            modifier = Modifier
-                                .padding(start = 10.dp, end = 10.dp, bottom = 20.dp),
-                            textAlign = TextAlign.Center,
-                            fontSize = 20.sp,
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = TextStyle(fontStyle = FontStyle.Normal),
-                            overflow = TextOverflow.Ellipsis
-                        )
+                        Row(
+                            modifier = Modifier,
+                            horizontalArrangement = Arrangement.Start,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                modifier = Modifier
+                                    .padding(start = 10.dp)
+                                    .size(10.dp),
+                                painter = painterResource(id = R.drawable.point_green),
+                                contentDescription = null
+                            )
+                            Text(
+                                text = "沉莫",
+                                modifier = Modifier
+                                    .padding(10.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = TextStyle(fontStyle = FontStyle.Normal)
+                            )
+                        }
                     }
                 }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...我失恋了...",
+                        modifier = Modifier
+                            .padding(start = 10.dp, end = 10.dp, bottom = 20.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        style = TextStyle(fontStyle = FontStyle.Normal),
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            }
+            var isThumbsUp by remember { mutableStateOf(false) }
             Row(
                 modifier = Modifier,
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-
+                if (isThumbsUp) {
+                    Image(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .size(30.dp)
+                            .clickable {
+                                isThumbsUp = !isThumbsUp
+                            },
+                        painter = painterResource(id = R.drawable.thumbs_up),
+                        contentDescription = null
+                    )
+                } else {
+                    Icon(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .size(30.dp)
+                            .clickable {
+                                isThumbsUp = !isThumbsUp
+                            },
+                        painter = painterResource(id = R.drawable.thumbs_up),
+                        contentDescription = null
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/syc/world/MainActivity.kt
+++ app/src/main/java/com/syc/world/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.syc.world.ui.theme.SYCWorldTheme
@@ -23,18 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             SYCWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.Start,
-                        verticalArrangement = Arrangement.Top
-                    ) {
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        LatestContentShow()
-                    }
+                    Home()
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    LatestContentShow()
                 }
             }
         }
