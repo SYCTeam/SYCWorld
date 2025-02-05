417c264cdb4aec574881eadecc5bf650464cfde8
suqi8 <3383787570@qq.com>
2025-02-05 18:54:16 +0800

    feat: Update Markdown Image Transformer to Coil3 and Refactor Dependencies
    - Updated the Markdown image transformer from `Coil2ImageTransformerImpl` to `Coil3ImageTransformerImpl` in `Publish_Dynamic.kt`, `Dynamic.kt`, and `Moments.kt`.
    - Modified `app/build.gradle.kts` to use `multiplatform-markdown-renderer-coil3` instead of `multiplatform-markdown-renderer-coil2`.
    - Refactored dependencies in `gradle/libs.versions.toml`, removing specific versions for markdown renderers and using lib references instead.
    - Wrapped the Markdown component in `Dynamic.kt` and `Publish_Dynamic.kt` with Column.

--- app/src/main/java/com/syc/world/Dynamic.kt
+++ app/src/main/java/com/syc/world/Dynamic.kt
@@ -8,9 +8,11 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -18,6 +20,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
@@ -42,7 +45,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -69,6 +80,7 @@ import dev.snipme.highlights.model.SyntaxThemes
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
+import top.yukonga.miuix.kmp.basic.CardDefaults
 import top.yukonga.miuix.kmp.basic.LazyColumn
 import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
 import top.yukonga.miuix.kmp.basic.Scaffold
@@ -224,6 +236,28 @@ fun Dynamic(navController: NavController,postId: Int,hazeState: HazeState,hazeSt
                 }
             }
         )
+    }, bottomBar = {
+        Row(
+            modifier = Modifier
+                .height(48.dp)
+                .fillMaxWidth()
+                .drawBehind {
+                    val shadowHeight = 4.dp.toPx()
+                    drawRect(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Black.copy(alpha = 0.2f),
+                                Color.Transparent
+                            ),
+                            startY = 0f,
+                            endY = shadowHeight
+                        ),
+                        topLeft = Offset(0f, -shadowHeight),
+                        size = Size(size.width, shadowHeight)
+                    )
+                }
+                .background(CardDefaults.DefaultColor())
+        ) { /* 内容 */ }
     }) { padding ->
         LazyColumn(
             contentPadding = PaddingValues(top = padding.calculateTopPadding()),
